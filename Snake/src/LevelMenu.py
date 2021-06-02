799e31a07a8b02533b4801c9e748c2f66f7f2a54
Joaquin Fontela <jfontela@fi.uba.ar>
2021-06-02 01:34:33 -0300

    add Level menu

--- Snake/src/LevelMenu.py
+++ Snake/src/LevelMenu.py
@@ -1,9 +1,7 @@
 import pygame
-from pygame.constants import KEYUP
 from TextFormatter.TextFormatter import TextFormatter
 from Config.configParser import CONFIGS
-import LevelMenu
-import SettingsMenu
+import Game
 import time
 
 THEME = CONFIGS["current_theme"]
@@ -15,6 +13,7 @@ FONT = "Fonts/retro.ttf"
 
 CHANGE_SELECTED_OPTION_SOUND_PATH = CONFIGS["sounds"]["change_selected_option"]
 SELECT_OPTION_SOUND_PATH = CONFIGS["sounds"]["select_option"]
+SELECT_START_GAME_SOUND_PATH = CONFIGS["sounds"]["select_start_game"]
 pygame.mixer.init()
 CHANGE_SELECTED_OPTION_SOUND = pygame.mixer.Sound(
     CHANGE_SELECTED_OPTION_SOUND_PATH)
@@ -22,9 +21,12 @@ CHANGE_SELECTED_OPTION_SOUND.set_volume(0.5)
 SELECT_OPTION_SOUND = pygame.mixer.Sound(
     SELECT_OPTION_SOUND_PATH)
 SELECT_OPTION_SOUND.set_volume(0.5)
+SELECT_START_GAME_SOUND = pygame.mixer.Sound(
+    SELECT_START_GAME_SOUND_PATH)
+SELECT_START_GAME_SOUND.set_volume(0.5)
 
 
-class MainMenu:
+class LevelMenu:
 
     def __init__(self, window):
         self.window = window
@@ -34,26 +36,31 @@ class MainMenu:
         self._loop()
 
     def _displayTexts(self):
-        '''
-        Displays the text in the main menu.
-        '''
         self.window.screen.fill(BACKGROUND_COLOR)
-        title = TextFormatter().formatText("PySnake", FONT, 150, LOGO_COLOR)
-        playButton = TextFormatter().formatText(
-            "PLAY", FONT, 100, SELECTED_COLOR if self.buttonSelected % 3 == 1 else NON_SELECTED_COLOR)
-        settingsButton = TextFormatter().formatText(
-            "SETTINGS", FONT, 100, SELECTED_COLOR if self.buttonSelected % 3 == 2 else NON_SELECTED_COLOR)
-        quitButton = TextFormatter().formatText(
-            "QUIT", FONT, 100, SELECTED_COLOR if self.buttonSelected % 3 == 0 else NON_SELECTED_COLOR)
+        title = TextFormatter().formatText("SELECT LEVEL:", FONT, 150, LOGO_COLOR)
+        level1Title = TextFormatter().formatText("1", FONT, 100,
+                                                 SELECTED_COLOR if self.buttonSelected % 5 == 1 else NON_SELECTED_COLOR)
+        level2Title = TextFormatter().formatText("2", FONT, 100,
+                                                 SELECTED_COLOR if self.buttonSelected % 5 == 2 else NON_SELECTED_COLOR)
+        level3Title = TextFormatter().formatText("3", FONT, 100,
+                                                 SELECTED_COLOR if self.buttonSelected % 5 == 3 else NON_SELECTED_COLOR)
+        level4Title = TextFormatter().formatText("4", FONT, 100,
+                                                 SELECTED_COLOR if self.buttonSelected % 5 == 4 else NON_SELECTED_COLOR)
+        level5Title = TextFormatter().formatText("5", FONT, 100,
+                                                 SELECTED_COLOR if self.buttonSelected % 5 == 0 else NON_SELECTED_COLOR)
         screenWidth, _ = self.window.getDimensions()
         self.window.screen.blit(
             title, (screenWidth/2 - (title.get_rect()[2]/2), 80))
         self.window.screen.blit(
-            playButton, (screenWidth/2 - (playButton.get_rect()[2]/2), 325))
+            level1Title, (screenWidth/6, 400))
         self.window.screen.blit(
-            settingsButton, (screenWidth/2 - (settingsButton.get_rect()[2]/2), 450))
+            level2Title, (2 * screenWidth/6, 400))
         self.window.screen.blit(
-            quitButton, (screenWidth/2 - (quitButton.get_rect()[2]/2), 575))
+            level3Title, (3 * screenWidth/6, 400))
+        self.window.screen.blit(
+            level4Title, (4 * screenWidth/6, 400))
+        self.window.screen.blit(
+            level5Title, (5 * screenWidth/6, 400))
         pygame.display.update()
 
     def _loop(self):
@@ -65,18 +72,14 @@ class MainMenu:
         while self.open:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key in (pygame.K_DOWN, pygame.K_UP):
-                        self._switchPlaySelected(event.key)
+                    if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+                        self._changeLevelSelected(event.key)
                         self._displayTexts()
                     if event.key == pygame.K_RETURN:
-                        if self.buttonSelected % 3 == 1:
-                            pygame.mixer.Sound.play(
-                                SELECT_OPTION_SOUND)
-                            LevelMenu.LevelMenu(self.window)
-                        elif self.buttonSelected % 3 == 2:
-                            pygame.mixer.Sound.play(
-                                SELECT_OPTION_SOUND)
-                            SettingsMenu.SettingsMenu(self.window)
+                        pygame.mixer.Sound.play(
+                            SELECT_START_GAME_SOUND)
+                        level = self.buttonSelected % 5
+                        Game.Game(self.window, level if level > 0 else 5)
                 if event.type == pygame.QUIT or \
                         (event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN and self.buttonSelected % 3 == 0):
                     pygame.mixer.Sound.play(SELECT_OPTION_SOUND)
@@ -84,9 +87,9 @@ class MainMenu:
                     self.open = False
                     self.window.stopRunning()
 
-    def _switchPlaySelected(self, eventKey):
+    def _changeLevelSelected(self, eventKey):
         pygame.mixer.Sound.play(CHANGE_SELECTED_OPTION_SOUND)
-        if eventKey == pygame.K_DOWN:
-            self.buttonSelected += 1
-        else:
+        if eventKey == pygame.K_LEFT:
             self.buttonSelected -= 1
+        else:
+            self.buttonSelected += 1
